0934ce4b3ac563b524cbd93cb8c64ebb6f44c2c1
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-03-04 15:48:18 -0600

    finished game

--- public/javascript/logic.js
+++ public/javascript/logic.js
@@ -1,5 +1,48 @@
-// make board
-// make buttons with ids
-// on button click, push button id into player array
-// make winning combo arrays
-// run winning check against winning combo arrays
+'use strict'
+
+let player = 2
+let player1Array = []
+let player2Array = []
+
+const winningArrays = ['012', '345', '678', '036', '147', '258', '048', '246']
+
+
+// function turn(btn) {
+
+    if(player % 2 === 0){
+        document.getElementById(btn).value = 'X'
+        player += 1
+        player1Array.push(btn)
+        console.log(player1Array)
+
+        checkWinner(player1Array)
+
+    } else {
+        document.getElementById(btn).value = 'O'
+        player += 1
+        player2Array.push(btn)
+        console.log(player2Array)
+
+        checkWinner(player2Array)
+    }
+// }
+
+
+function checkWinner(dudeArray) {
+
+    let newarr = dudeArray.join('')
+    if(dudeArray.length === 3 && winningArrays.indexOf(newarr) !== -1) {
+        console.log('we have a winner!')
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
