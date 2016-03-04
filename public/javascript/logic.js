90cbdd808efdf073964566694ab8b9aa76d0d709
Brian Stumbaugh <brian.stumbuagh@gmail.com>
2016-03-04 11:16:12 -0600

    sass set up

---
+++ public/javascript/logic.js
@@ -0,0 +1,5 @@
+// make board
+// make buttons with ids
+// on button click, push button id into player array
+// make winning combo arrays
+// run winning check against winning combo arrays
--- server.js
+++ server.js
@@ -2,18 +2,34 @@
 
 const express = require('express')
 const app = express()
+const path = require('path')
 
 const PORT = process.env.PORT || 3000
 
 app.set('view engine', 'jade')
+
+// this is a middleware function for sass
+app.use(require('node-sass-middleware')({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    indentedSyntax: true,
+    sourceMap: true
+}))
+
 app.use(express.static('public'))
 
+// this one gives a path to anything in public that doesn't already have a path
+app.use(express.static(path.join(__dirname, 'public')));
+// this one gives a path to anything in views that doesn't already have a path
+app.set('views', path.join(__dirname, 'views'));
 
+
+// rendering main page
 app.get('/', (req, res) => {
     res.render('index')
 })
 
-
+// server set up
 app.listen(PORT, () => {
     console.log(`we hear you server on port: ${PORT}`)
 })
